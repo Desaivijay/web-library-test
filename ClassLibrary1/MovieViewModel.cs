00c07a188562c1e21da9e1a1b90c87160168bbe0
Vijaybhai Desai <47266239+Desaivijay@users.noreply.github.com>
2023-08-11 22:22:58 -0400

    library test file

---
+++ ClassLibrary1/MovieViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace tmdb_web_application.Models
+{
+    public class MovieViewModel
+    {
+        public string Title { get; set; }
+        public string Overview { get; set; }
+        public string ReleaseDate { get; set; }
+    }
+}
